78c04374867052c49507ba32db17e056dadbd211
Ian Soboroff <ian.soboroff@nist.gov>
2021-01-03 18:09:13 -0500

    Adding pool loading

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,11 +6,12 @@ import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [username, set_username] = useState('');
+  const [username, set_username] = useState(null);
   const [login_required, set_login_required] = useState(true);
   const [topic, set_topic] = useState(-1);
   const [current, set_current] = useState(-1);
@@ -22,9 +23,24 @@ function App() {
   };
 
   const load_topic = () => {
-    console.log('load_topic');
+    if (username === null)
+      return;
+
+    const url = 'get_pool?u=' + username + '&t=' + topic;
+    fetch(url)
+      .then((response) => {
+	if (response.ok) {
+	  return response.json();
+	} else {
+	  throw Error(response.statusText);
+	}
+      }).then((data) => {
+        set_topic(data.topic);
+        set_current(0);
+        set_pool(data.pool);
+      });
   };
-  
+
   return (
     <>
       <Modal show={login_required} onHide={do_login}
@@ -68,15 +84,17 @@ function App() {
           </Form>		  
         </div>
       </Navbar>
-      
-      <Row className="mx-5 mt-5">
-        <Col style={{overflowY: 'scroll'}}>
-          <p> This is the pool view </p>
-        </Col>
-        <Col>
-          <p> This is the document view</p>
-        </Col>
-      </Row>
+
+      <Container fluid>
+        <Row className="mx-5 mt-5">
+          <Col xs={4} style={{overflowY: 'scroll'}}>
+            <p> This is the pool view </p>
+          </Col>
+          <Col>
+            <p> This is the document view</p>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
