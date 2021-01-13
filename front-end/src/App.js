74cd63fcad05d8826d88bde1141fbdec7132ce2a
Ian Soboroff <ian.soboroff@nist.gov>
2021-01-13 14:36:16 -0500

    Judgments updating pool view

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,11 +8,59 @@ import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Badge from 'react-bootstrap/Badge';
 
 import BetterDocument from './BetterDocument';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+const rel_levels = {'0': {label: 'irrelevant',  color: 'secondary'},
+                    '1': {label: 'topical',     color: 'info'},
+                    '2': {label: 'significant', color: 'primary'},
+                    '3': {label: 'decisional',  color: 'success'},
+                    '4': {label: 'DECISIVE',    color: 'danger'}};
+
+function PoolItem(props) {
+  const load_doc = (docid, seq) => {
+    const url = 'doc?u=foo&d=' + docid;
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw Error(response.statusText);
+        }
+      }).then((data) => {
+        // put the document data where it should go.
+        props.setDoc(data);
+        props.setCurrent(seq);
+      });
+  };
+
+  let badge = '';
+  if (props.judgment !== '-1') {
+    badge = (<Badge variant={rel_levels[props.judgment].color}>
+               {rel_levels[props.judgment].label}
+             </Badge>);
+  }
+  return (
+    <ListGroup.Item action
+                    active={props.current}
+                    onClick={() => load_doc(props.docid, props.seq)}>
+      {props.seq}: {props.docid} {badge}
+    </ListGroup.Item>
+  );
+}
+
+function Pool(props) {
+  const entries =  props.pool.map((entry, i) => (
+    <PoolItem docid={entry.docid} seq={i} judgment={entry.judgment}
+              current={props.current === i}
+              setDoc={props.setDoc} setCurrent={props.setCurrent}/>
+  ));
+  return (<ListGroup> {entries} </ListGroup>);
+}
 
 function App() {
   const [username, set_username] = useState('');
@@ -22,19 +70,6 @@ function App() {
   const [pool, set_pool] = useState([]);
   const [doc, set_doc] = useState('');
 
-  const btn_labels = [['0', 'irrelevant'],
-                      ['1', 'topical'],
-                      ['2', 'significant'],
-                      ['3', 'decisional'],
-                      ['4', 'DECISIVE']];
-  
-  const color_mapping = [['0', 'secondary'],
-                         ['1', 'info'],
-                         ['2', 'primary'],
-                         ['3', 'success'],
-                         ['4', 'danger']];
-  const rel_colors = new Map(color_mapping);
-  
   const do_login = () => {
     if (username !== null)
       set_login_required(false);
@@ -59,47 +94,30 @@ function App() {
       });
   };
 
-  const load_doc = (docid, i) => {
-    if (username === null)
-      return;
-
-    const url = 'doc?u=' + username + '&d=' + docid;
-    fetch(url)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw Error(response.statusText);
-        }
-      }).then((data) => {
-        // put the document data where it should go.
-        set_doc(data);
-        set_current(i);
-      });
-  };
-
+  const set_judgment = (index, level) => {
+    let newPool = pool.map((item, i) => {
+      if (index === i) {
+        return { ...item, judgment: level };
+      } else {
+        return item;
+      }
+    });
+    set_pool(newPool);
+  };                      
+  
   const judge_current = (level) => {
-    pool[current].judgment = level;
+    set_judgment(current, level);
     // Oh and send something back to the server willya?
   };
 
-  const judgment_buttons = btn_labels.map((entry) => (
+  const judgment_buttons = Object.getOwnPropertyNames(rel_levels).map((i) => (
     <ListGroup.Item action
-                    variant={rel_colors.get(entry[0])}
-                    onClick={() => judge_current(entry[0])}>
-      {entry[1]}
+                    variant={rel_levels[i].color}
+                    onClick={() => judge_current(i)}>
+      {rel_levels[i].label}
     </ListGroup.Item>
   ));
 
-  const pool_list = pool.map((entry, i) => (
-    <ListGroup.Item action
-                    active={current === i}
-                    variant={rel_colors.get(entry.judgment) || null}
-                    onClick={() => load_doc(entry.docid, i)}>
-      {entry.docid}: {entry.judgment}
-    </ListGroup.Item>
-  ));
-  
   return (
     <>
       <Modal show={login_required} onHide={do_login}
@@ -152,9 +170,7 @@ function App() {
       <Container className='mx-3 mt-5'>
         <Row className='mt-5'>
           <Col xs={4} style={{overflowY: 'scroll'}}>
-            <ListGroup>
-              { pool_list }
-            </ListGroup>
+            <Pool pool={pool} current={current} setDoc={set_doc} setCurrent={set_current}/>
           </Col>
           <Col>
             <BetterDocument content={doc}/>
