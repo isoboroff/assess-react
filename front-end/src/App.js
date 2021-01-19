86c95e683e0d0ad798bae6e086d71421fd6c4c43
Ian Soboroff <ian.soboroff@nist.gov>
2021-01-18 20:24:30 -0500

    Logins, loading pool, loading doc

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -11,7 +11,6 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Badge from 'react-bootstrap/Badge';
 
 import BetterDocument from './BetterDocument';
-import { useLocalStorage } from './hooks';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -22,119 +21,54 @@ const rel_levels = {'0': {label: 'irrelevant',  color: 'secondary'},
                     '2': {label: 'significant', color: 'primary'},
                     '3': {label: 'decisional',  color: 'success'},
                     '4': {label: 'DECISIVE',    color: 'danger'}};
-/*
- * A tricky thing about React Hooks is that they have to be used
- * at the "top level", or from within another hook, otherwise it
- * causes weird stuff in the render loop.  A major impact is that
- * stuff that fetches data from the backend needs to be wrapped in
- * it's own custom hook.  I haven't found a nice pattern for these.
- *
- * So, this custom hook is about getting documents.  When the 
- * docid value is set, then the document gets fetched.  The
- * doc state value can be passed to an interface prop.
- */
-const useDocApi = (username) => {
-  const [doc, setDoc] = useState('');
-  const [docid, setDocid] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
-      const url = 'doc?u=' + username + '&d=' + docid;
-      
-      try {
-        const response = await fetch(url);
-        const content = await response.json();
-        setDoc(content);
-      } catch (error) {
-        setIsError(true);
-      }
-
-      setIsLoading(false);
-    };
-
-    if (username && docid)
-      fetchData();
-  }, /* depends on */ [docid]);
-
-  return [{ doc, isLoading, isError }, setDocid];
+const initial_state = {
+  username: '',
+  current: -1,
+  cur_doc: '',
+  topic: '',
+  pool: []
 };
 
-/*
- * This custom hook is for loading the pool and judging
- * documents.  Both of these actions affect the pool
- * state, and so I combined both activities in one custom
- * hook.
- *
- * Setting the topic triggers a pool load.  Setting the
- * judgment triggers a judgment-action, which is applied
- * both in the local pool and is sent to the back-end for
- * logging.
- */
-const usePoolApi = (username, initTopic) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [topic, setTopic] = useState(initTopic);
-  const [pool, setPool] = useState([]);
-  const [judgment, setJudgment] = useState({});
-
-  // Pool load effect
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
-      const url = 'pool?u=' + username + '&t=' + topic;
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        setPool(data.pool);
-      } catch (error) {
-        setIsError(true);
-      }
-
-      setIsLoading(false);
-    };
-
-    if (username && topic != -1)
-      fetchData();
-  }, /* depends on */ [topic]);
-
-  // Document judgment effect
-  useEffect(() => {
-    const sendJudgment = async () => {
-      setIsError(false);
-      setIsLoading(true);
-      const url = 'judge?u=' + username
-            + '&t=' + topic
-            + '&d=' + pool[judgment.index].docid
-            + '&j=' + judgment.level;
-      try {
-        const response = await fetch(url, { method: 'POST' });
-        if (await response.ok) {
-          let newPool = pool.map((item, i) => {
-            if (judgment.index === i) {
-              return { ...item, judgment: judgment.level };
-            } else {
-              return item;
-            }
-          });
-          setPool(newPool);
-        }
-      } catch (error) {
-        setIsError(true);
-      }
-      setIsLoading(false);
-    };
-
-    if (judgment && judgment.index > 0 && judgment.index < pool.length)
-      sendJudgment();
-  }, /* depends on */ [judgment]);
-
-  return [{ pool, isLoading, isError }, setTopic, setJudgment];
+const States = Object.freeze({
+  LOGIN: 'LOGIN',
+  LOAD_POOL: 'LOAD_POOL',
+  FETCH_DOC: 'FETCH_DOC',
+  JUDGE: 'JUDGE',
+});
+  
+function assess_reducer(state, action) {
+  switch (action.type) {
+  case States.LOGIN:
+    return {...state,
+            username: action.payload.username};
+  case States.LOAD_POOL:
+    return {...state,
+            topic: action.payload.topic,
+            current: 0,
+            pool: action.payload.pool};
+  case States.FETCH_DOC:
+    return {...state,
+            current: action.payload.current,
+            doc: action.payload.doc};
+  case States.JUDGE:
+    const newPool = state.pool.map((entry) => {
+      if (entry.docid === action.payload.docid)
+        return {...entry,
+                level: action.payload.level};
+      else
+        return entry;
+    });
+    return {...state,
+            pool: newPool};
+  default:
+    return state;
+  }
 };
+
+
+
+const AssessDispatch = React.createContext(null);
 
 /*
  * A pool item interface component.  Clicking a pool item causes it to
@@ -149,16 +83,21 @@ function PoolItem(props) {
                {rel_levels[props.judgment].label}
              </Badge>);
   }
-  const do_fetch_and_set = (docid, index) => {
-    props.fetchDoc(docid);
-    props.setCurrent(index);
-  };
-  
+  const dispatch = useContext(AssessDispatch);
+  function fetch_doc(seq, docid) {
+    fetch('doc?d=' + docid)
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: States.FETCH_DOC,
+                   payload: { current: seq,
+                              doc: data }});
+      });
+  }
   return (
     <ListGroup.Item action
                     active={props.current}
-                    onClick={() => do_fetch_and_set(props.docid, props.seq)}>
-      {props.seq}: {props.docid} {badge}
+                    onClick={() => fetch_doc(props.seq, props.docid)}>
+      {props.seq + 1}: {props.docid} {badge}
     </ListGroup.Item>
   );
 }
@@ -169,10 +108,43 @@ function PoolItem(props) {
 function Pool(props) {
   const entries =  props.pool.map((entry, i) => (
     <PoolItem docid={entry.docid} seq={i} judgment={entry.judgment}
-              current={props.current === i}
-              fetchDoc={props.fetchDoc} setCurrent={props.setCurrent}/>
+              current={props.current === i}/>
   ));
   return (<ListGroup> {entries} </ListGroup>);
+}
+
+function LoginModal(props) {
+  const [username, set_username] = useState('');
+  const dispatch = useContext(AssessDispatch);
+
+  function do_login() {
+    dispatch({type: States.LOGIN, payload: {username: username}});
+    props.set_required(false);
+  }
+  
+  return (
+    <Modal show={props.login_required} onHide={do_login}
+           backdrop="static" keyboard={false}>
+      <Modal.Header>
+        <Modal.Title>Please log in</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form.Control type="text"
+                      placeholder="user"
+                      value={username}
+                      onChange={(e) => set_username(e.target.value)}
+                      onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                          e.preventDefault();
+                          e.stopPropagation();
+                          do_login();
+                        }}}/>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={() => do_login()}>Log in</Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 /*
@@ -182,35 +154,29 @@ function Pool(props) {
  * BetterDocument component handles document rendering.
  */
 function App() {
-  const [username, set_username] = useLocalStorage('username', '');
-  const [login_required, set_login_required] = useState(true);
-  const [topic, set_topic] = useLocalStorage('topic', -1);
-  const [current, set_current] = useLocalStorage('current', -1);
-  const [{ pool, poolIsLoading, poolError }, load_pool, do_judge] = usePoolApi(username, -1);
-  const [{ doc, docIsLoading, docIsError}, fetch_doc] = useDocApi(username);
+  const [state, dispatch] = useReducer(assess_reducer, initial_state);
+  const [login_required, set_login_required] = useState(false);
+  const [topic_entry, set_topic_entry] = useState('');
+  const [load_pool, set_load_pool] = useState(false);
 
-  const do_login = () => {
-    if (username !== '')
-      set_login_required(false);
-  };
-
-  const judge_current = (level) => {
-    do_judge({index: current, level: level});
-  };
-
+  /* Effect to fire just before initial render */
   useEffect(() => {
-    set_login_required(username === '');  
-    load_pool(topic);
+    if (state.username === '')
+      set_login_required(true);
   }, []);
 
-  let current_docid = '';
-  if (current >= 0 && current < pool.length) {
-    current_docid = pool[current].docid;
-  }
-  
   useEffect(() => {
-    fetch_doc(current_docid);
-  }, [pool]);
+    if (!load_pool)
+      return;
+    
+    fetch('pool?u=' + state.username + '&t=' + topic_entry)
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: States.LOAD_POOL, payload: { topic: topic_entry,
+                                                      pool: data.pool}});
+        set_load_pool(false);
+      });
+  }, [load_pool, topic_entry]);
   
   /*
    * The judgment buttons are colored according to the key at the top,
@@ -218,13 +184,15 @@ function App() {
    */
   const judgment_buttons = Object.getOwnPropertyNames(rel_levels).map((i) => {
     let style = 'font-weight-normal';
-    if (current >= 0 && current < pool.length && i === pool[current].judgment) {
+    if (state.current >= 0 &&
+        state.current < state.pool.length
+        && i === state.pool[state.current].judgment) {
       style = 'font-weight-bold';
     }
     return (
       <ListGroup.Item action
                       variant={rel_levels[i].color}
-                      onClick={() => judge_current(i)}>
+                      onClick={() => console.log('judge_current('+i+')')}>
         <span className={style}>
           {rel_levels[i].label}
         </span>
@@ -233,65 +201,49 @@ function App() {
   });
   
   return (
-    <Container fluid className='overflow-hidden'>
-      <Modal show={login_required} onHide={do_login}
-             backdrop="static" keyboard={false}>
-        <Modal.Header>
-          <Modal.Title>Please log in</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form.Control type="text"
-                        placeholder="user"
-                        value={username}
-                        onChange={(e) => set_username(e.target.value)}
-                        onKeyDown={(e) => {
-                          if (e.key === 'Enter') {
-                            e.preventDefault();
-                            e.stopPropagation();
-                            do_login();
-                          }}}/>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={do_login}>Log in</Button>
-        </Modal.Footer>
-      </Modal>
+    <AssessDispatch.Provider value={dispatch}>
+      <Container fluid className='overflow-hidden'>
+        
+        <LoginModal login_required={login_required} set_required={set_login_required}/>
 
-      <Navbar fixed="top" className="bg-light">
-        <div className="">
-          <Form inline>
-            <Form.Control placeholder="Topic" className="col-3"
-                          value={topic}
-                          onChange={(e) => set_topic(e.target.value)}
-                          onKeyDown={(e) => {
-                            if (e.key === 'Enter') {
-                              e.preventDefault();
-                              e.stopPropagation();
-                              load_pool(topic);
-                            }}}/>
-            <Button variant="primary" onClick={() => load_pool(topic)}>Load</Button>
-            <div className="p-2">
-              {username} &nbsp; {current + 1} of {pool.length}
-            </div>
-          </Form>		  
-        </div>
-        <div>
-          <ListGroup horizontal>
-            {judgment_buttons}
-          </ListGroup>
-        </div>
-      </Navbar>
+        <Navbar fixed="top" className="bg-light">
+          <div className="">
+            <Form inline>
+              <Form.Control placeholder="Topic" className="col-3"
+                            value={topic_entry}
+                            onChange={(e) => set_topic_entry(e.target.value)}
+                            onKeyDown={(e) => {
+                              if (e.key === 'Enter') {
+                                e.preventDefault();
+                                e.stopPropagation();
+                                set_load_pool(true);
+                              }}}/>
+              <Button variant="primary"
+                      onClick={() => set_load_pool(true)}>Load</Button>
+              <div className="p-2">
+                {state.username} &nbsp; {state.current + 1} of {state.pool.length}
+              </div>
+            </Form>		  
+          </div>
+          <div>
+            <ListGroup horizontal>
+              {judgment_buttons}
+            </ListGroup>
+          </div>
+        </Navbar>
 
-      <Container fluid className='mx-3 mt-5 h-100 overflow-hidden'>
-        <Row className='mt-5 vh-100 overflow-hidden'>
-          <Col xs={4} className='vh-100 overflow-auto'>
-            <Pool pool={pool} current={current} fetchDoc={fetch_doc} setCurrent={set_current}/>
-          </Col>
-          <Col className='vh-100 mr-3 overflow-auto'>
-            <BetterDocument content={doc} loading={docIsLoading} error={docIsError}/>
-          </Col>
-        </Row>
+        <Container fluid className='mx-3 mt-5 h-100 overflow-hidden'>
+          <Row className='mt-5 vh-100 overflow-hidden'>
+            <Col xs={4} className='vh-100 overflow-auto'>
+              <Pool pool={state.pool} current={state.current}/>
+            </Col>
+            <Col className='vh-100 mr-3 overflow-auto'>
+              <BetterDocument content={state.doc}/>
+            </Col>
+          </Row>
+        </Container>
       </Container>
-    </Container>
+    </AssessDispatch.Provider>
   );
 }
 
