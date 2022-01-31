dc02a10d98ae0e085fcce1766b315d0952cb7cf1
Ian Soboroff <isoboroff@gmail.com>
2022-01-31 09:28:50 -0500

    Document rendering

--- front-end/src/Highlightable.js
+++ front-end/src/Highlightable.js
@@ -127,19 +127,15 @@ function Highlightable(props) {
   // but with added bits to support highlighting
   //
   const display_doc = () => {
-    if (parsed === null)
-      return '';
-
-    if (!(parsed && parsed.hasOwnProperty('derived-metadata')))
-      return '';
-
-    let meta = parsed['derived-metadata'];
-    let text = meta['text'];
+    let text = content['text'];
     if (props.rel)
       text = highlight_rel_passage(text);
 
     return (
       <div>
+        { content['id'] }</br>
+        <h1> { content['title'] } </h1>
+        <h3> { content['date'] } </h3>
         <div dir="rtl" className="text-right article-text"
              onMouseUp={() => {
                if (has_selection()) {
@@ -154,8 +150,7 @@ function Highlightable(props) {
     );
   };
 
-  if (props.content && props.content.hasOwnProperty('orig')) {
-    parsed = JSON.parse(props.content['orig']);
+  if (props.content && props.content.hasOwnProperty('text')) {
     return display_doc();
   } else {
     return <p>waiting...</p>;
