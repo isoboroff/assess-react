c0a16c9eec241fad3846d5036b82f8f17c491b33
Ian Soboroff <isoboroff@gmail.com>
2022-02-01 13:58:26 -0500

    Remove extra topic bits

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -255,28 +255,6 @@ function Description(props) {
         <span className="h2 mr-5">Topic: {props.desc['topic_id']}</span><br/>
         <p><b>Title:</b> {props.desc['topic_title']}</p>
         <p><b>Description:</b> {props.desc['topic_description']}</p>
-        <Button variant="link"
-          onClick={() => setShow(!show)}
-          aria-controls="lang-resources"
-          aria-expanded={show}>
-          Show details
-        </Button>
-        <Collapse in={show}>
-          <div id="lang-resources">
-            <dl>
-              <dd>{props.desc['rus']['topic_title']}</dd>
-              <dt>Description:</dt><dd>{props.desc['rus']['topic_description']}</dd>
-              <dt>Very valuable:</dt>
-              <dd>{props.desc['rus']['narrative_very_valuable']}</dd>
-              <dt>Somewhat valuable:</dt>
-              <dd> {props.desc['rus']['narrative_somewhat_valuable']}</dd>
-              <dt>Not that valuable:</dt>
-              <dd> {props.desc['rus']['narrative_not_that_valuable']}</dd>
-              <dt>Not relevant:</dt>
-              <dd> {props.desc['rus']['narrative_non_relevant']}</dd>
-            </dl>
-          </div>
-        </Collapse>
       </div>
     );
   } else {
