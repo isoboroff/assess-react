aeb89226454a9d30bb88bcf4c03a4b31137a2b13
Ian Soboroff <isoboroff@gmail.com>
2022-07-26 13:59:43 -0400

    Moving configuration to a config file and getting rid of argparse

---
+++ back-end/settings.py
@@ -0,0 +1,14 @@
+# ElasticSearch host
+ELASTIC_HOST='localhost'
+
+# ElasticSearch port
+ELASTIC_PORT=9200
+
+# Directory for saving assessor work
+SAVE='relevant'
+
+# ElasticSearch index
+INDEX='hc4'
+
+# Password file
+PWFILE='passwd'
