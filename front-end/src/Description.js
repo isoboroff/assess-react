3ce600e301850b4bcde43cdfb15b8e5114d1479e
Ian Soboroff <isoboroff@gmail.com>
2023-07-31 17:31:21 -0400

    Displaying the AToMiC descriptions.  These are really long.

--- front-end/src/Description.js
+++ front-end/src/Description.js
@@ -2,22 +2,40 @@ import { useState } from 'react';
 
 // Render the task/request description
 function Description(props) {
-  const [show, setShow] = useState(false);
+
+  function Hideaway(props) {
+    const [show, setShow] = useState(false);
+
+    function handleClick() {
+      setShow(!show);
+    }
+
+    return (
+      <div>
+        <span className="h3 mr-5">{props.title}</span>
+        <button onClick={handleClick}>{show ? 'hide' : 'show'}</button>
+        <p style={{ display: show ? '' : 'none' }}>
+          {props.children}
+        </p>
+      </div>
+    );
+  }
 
   if (props.desc) {
-    const desc = props.desc.topics[0];
     return (
       <div className="border-bottom">
-        <span className="h2 mr-5">Request: {props.desc['topic_id']}</span><br />
-        <p><b>{desc['topic_title']}</b></p>
-        <p>{desc['topic_description']}</p>
-        <p>{desc['topic_narrative']}</p>
+        <p><b>{props.desc['text_id']}</b></p>
+        <Hideaway title={props.desc['page_title']}>
+          <p id='page_desc' style={{ whiteSpace: 'pre-wrap' }}>{props.desc['context_page_description']}</p>
+        </Hideaway>
+        <span className="h3 mr-5">{props.desc['section_title']}</span>
+        <p style={{ whiteSpace: 'pre-wrap' }}>{props.desc['context_section_description']}</p>
       </div>
     );
   } else {
     return null;
   }
-  // <p style={{whiteSpace: 'pre-wrap'}}>{desc}</p>
+  // <p style={{ whiteSpace: 'pre-wrap' }}>{desc}</p>
 }
 
 export default Description;
