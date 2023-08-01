1f5d894aafc1f2d586a48cd1d3c9b39e8fa617c9
Ian Soboroff <isoboroff@gmail.com>
2023-07-31 22:03:39 -0400

    Add captions to images

--- front-end/src/RawImage.js
+++ front-end/src/RawImage.js
@@ -4,8 +4,11 @@ function RawImage(props) {
 
   if (props.data) {
     return (
-      <img id="rawimage" src={"data:image/" + props.data.img_type + ";base64," + props.data.image}
-        alt={props.data.attrib && props.data.attrib[0]} />
+      <figure>
+        <img id="rawimage" src={"data:image/" + props.data.img_type + ";base64," + props.data.image}
+          alt={props.data.attrib && props.data.attrib[0]} />
+        <figcaption>{props.data.attrib && props.data.attrib[0]}</figcaption>
+      </figure>
     );
   } else {
     return "";
