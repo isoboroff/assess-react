b2c225b1870448029f88ea87f5baa43efba5e44f
soboroff <ian.soboroff@nist.gov>
2024-09-09 17:22:05 -0400

    For the conversation modal, only show the conversation up until the turn we are assessing.  Change the button to match.  Oh we have also started using prettier to do code formatting on save, so this if probably a large patch.

--- front-end/src/Description.js
+++ front-end/src/Description.js
@@ -1,50 +1,58 @@
-import { useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 function FullConvoModal(props) {
-  const turns = props.desc.turns.map((turn) => {
-    const bold = turn.turn_id === props.turn;
-    let text_class;
-    if (bold) {
-      text_class = "fw-bold";
-    } else {
-      text_class = "fw-light";
-    }
+  const turns = props.desc.turns
+    .filter((turn) => {
+      return turn.turn_id <= props.turn;
+    })
+    .map((turn) => {
+      const bold = turn.turn_id === props.turn;
+      let text_class;
+      if (bold) {
+        text_class = "fw-bold";
+      } else {
+        text_class = "fw-light";
+      }
 
-    return (
-      <div className="border-bottom">
-        <div className={text_class}>
-        <p>{turn.turn_id}: &nbsp;
-        {turn.utterance}<br/>
-      ({turn.resolved_utterance})</p>
-        <p>Sample response: {turn.response}</p>
+      return (
+        <div className="border-bottom">
+          <div className={text_class}>
+            <p>
+              {turn.turn_id}: &nbsp;
+              {turn.utterance}
+              <br />({turn.resolved_utterance})
+            </p>
+            {props.turn != turn.turn_id && (
+              <p>Sample response: {turn.response}</p>
+            )}
+          </div>
         </div>
-        </div>
-    )});
+      );
+    });
 
   return (
-      <Modal show={props.show}>
-        <Modal.Header>
-          <Modal.Title> Full conversation</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {turns}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={() => props.set_show(false)}>
-    Dismiss</Button>
-        </Modal.Footer>
-      </Modal>
+    <Modal show={props.show} onHide={() => props.set_show(false)} size="lg">
+      <Modal.Header>
+        <Modal.Title> Full conversation</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{turns}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={() => props.set_show(false)}>
+          Dismiss
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
-};
+}
 
 // Render the task/request description
 function Description(props) {
   const [show, set_show] = useState(false);
 
   if (props.desc) {
-    const turn_id = parseInt(props.topic.split('_')[1]);
+    const turn_id = parseInt(props.topic.split("_")[1]);
     let utterance = null;
     let resolved;
     for (const t of props.desc.turns) {
@@ -55,20 +63,33 @@ function Description(props) {
     }
     return (
       <>
-        <FullConvoModal show={show} set_show={set_show} desc={props.desc} turn={turn_id}/>
+        <FullConvoModal
+          show={show}
+          set_show={set_show}
+          desc={props.desc}
+          turn={turn_id}
+        />
         <div className="border-bottom">
-          <span className="mr-5">Conversation {props.desc.number}, turn {turn_id}</span>
+          <span className="mr-5">
+            Conversation {props.desc.number}, turn {turn_id}
+          </span>
           <button variant="secondary" onClick={() => set_show(true)}>
-            see full conversation
+            see conversation so far
           </button>
           <br />
-          <p><h2>{props.desc.title}</h2></p>
-          { utterance != null ?
-          <>
-          <p><b>{utterance}</b></p>
-          <p>({resolved})</p>
-          </>
-          : <p>Can't find turn {turn_id}</p> }
+          <p>
+            <h2>{props.desc.title}</h2>
+          </p>
+          {utterance != null ? (
+            <>
+              <p>
+                <b>{utterance}</b>
+              </p>
+              <p>({resolved})</p>
+            </>
+          ) : (
+            <p>Can't find turn {turn_id}</p>
+          )}
         </div>
       </>
     );
