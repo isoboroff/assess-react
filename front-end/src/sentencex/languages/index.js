9b631cb69c27e773af162dffd82dfd8fc7d22759
soboroff <ian.soboroff@nist.gov>
2025-07-27 19:37:33 -0400

    Add sentencex

---
+++ front-end/src/sentencex/languages/index.js
@@ -0,0 +1,63 @@
+import Amharic from './am.js'
+import Arabic from './ar.js'
+import Armenian from './hy.js'
+import Bengali from './bn.js'
+import Bulgarian from './bg.js'
+import Burmese from './my.js'
+import Catalan from './ca.js'
+import Danish from './da.js'
+import Deutsch from './de.js'
+import Dutch from './nl.js'
+import English from './en.js'
+import Finnish from './fi.js'
+import French from './fr.js'
+import Greek from './el.js'
+import Gujarati from './gu.js'
+import Hindi from './hi.js'
+import Japanese from './ja.js'
+import Kannada from './kn.js'
+import Kazakh from './kk.js'
+import Malayalam from './ml.js'
+import Marathi from './mr.js'
+import Odia from './or.js'
+import Polish from './pl.js'
+import Portuguese from './pt.js'
+import Punjabi from './pa.js'
+import Russian from './ru.js'
+import Slovak from './sk.js'
+import Spanish from './es.js'
+import Tamil from './ta.js'
+import Telugu from './te.js'
+
+export default {
+  am: Amharic,
+  ar: Arabic,
+  bg: Bulgarian,
+  bn: Bengali,
+  ca: Catalan,
+  da: Danish,
+  de: Deutsch,
+  el: Greek,
+  en: English,
+  es: Spanish,
+  fi: Finnish,
+  fr: French,
+  gu: Gujarati,
+  hi: Hindi,
+  hy: Armenian,
+  ja: Japanese,
+  kk: Kazakh,
+  kn: Kannada,
+  ml: Malayalam,
+  mr: Marathi,
+  my: Burmese,
+  nl: Dutch,
+  or: Odia,
+  pa: Punjabi,
+  pl: Polish,
+  pt: Portuguese,
+  ru: Russian,
+  sk: Slovak,
+  ta: Tamil,
+  te: Telugu
+}
