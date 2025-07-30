a217145608cfe47ed83b4dd6e6bfadf7a6df3250
soboroff <ian.soboroff@nist.gov>
2025-07-30 11:12:32 -0400

    This is the RAGTIME user.py file

--- back-end/user.py
+++ back-end/user.py
@@ -5,11 +5,12 @@ class User(UserMixin):
 
     """Custom User class."""
 
-    def __init__(self, username, name, email):
-        self.id = username
+    def __init__(self, username, name, email, dest):
+        self.id = email
         self.username = username
         self.name = name
         self.email = email
+        self.destination = dest
 
     def claims(self):
         """Use this method to render all assigned claims on profile page."""
@@ -17,21 +18,32 @@ class User(UserMixin):
                 'email': self.email}.items()
 
     @staticmethod
-    def get(id):
-        print(f'user:User.get({id})')
-        result = USERS_DB.get(id)
+    def get(email):
+        result = USERS_DB.get(email)
         return result
 
     def get_id(self):
-        print(f'user:User.get_id() -> {self.id}')
-        return self.id
+        return self.email
 
     @staticmethod
     def create(username, name, email):
-        USERS_DB[username] = User(username, name, email)
-        return USERS_DB[username]
+        USERS_DB[email] = User(username, name, email)
+        return USERS_DB[email]
 
 # Simulate user database
-USERS_DB = {
-    'ian': User('ian', 'Ian', 'ian.soboroff@nist.gov'),
+USERS_DB = { 
+    'darrin.dimmick@nist.gov': User('darrin', 'Darrin', 'darrin.dimmick@nist.gov', 'assess'),
+    'hoa.dang@nist.gov': User('hoa', 'Hoa', 'hoa.dang@nist.gov', 'assess'),
+    'ian.soboroff@nist.gov': User('ian', 'Ian', 'ian.soboroff@nist.gov', 'assess'),
+    'pechnikova@gmail.com': User('anna', 'Anna Pechnikova', 'pechnikova@gmail.com', 'assess'),
+    'hshaobo@gmail.com': User('shaobo', 'Huang Shaobo', 'hshaobo@gmail.com', 'assess'),
+    'eddy03161949@gmail.com': User('aassaad', 'Aayaad Assaad', 'eddy03161949@gmail.com', 'assess'),
+    'emtf8009@gmail.com': User('lucy', 'Lucy Wang', 'emtf8009@gmail.com', 'assess'),
+    'elenaworksathome@gmail.com': User('elena', 'Elena Prisekin', 'elenaworksathome@gmail.com', 'assess'),
+    'lchen1965@gmail.com': User('lijun', 'Lijun Chen', 'lchen1965@gmail.com', 'assess'),
+    'natallia.jones22@gmail.com': User('natallia', 'Natallia Cherashneva', 'natallia.jones22@gmail.com', 'assess'),
+    'mvrichaud@gmail.com': User('marina', 'Marina Richaud', 'mvrichaud@gmail.com', 'assess'),
+    'nada_fayad@hotmail.com': User('nada', 'Nada Fayad', 'nada_fayad@hotmail.com', 'assess'),
+    'davewoosley@gmail.com': User('dave', 'Dave Woosley', 'davewoosley@gmail.com', 'assess'),
+    'yanbahd43@gmail.com': User('peter', 'Peter Schultz', 'yanbahd43@gmail.com', 'assess')
 }
