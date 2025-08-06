bb733b3d912e7a63bbf0439db0acd5c0017afb72
soboroff <ian.soboroff@nist.gov>
2025-08-05 22:25:36 -0400

    Dialog editor comes up but doesn't display the current note.  I wonder if there is just too damn much useState going on

--- front-end/src/DocumentView.jsx
+++ front-end/src/DocumentView.jsx
@@ -1,45 +1,119 @@
 import { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 
+import Modal from "react-bootstrap/Modal";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col"
+import Stack from "react-bootstrap/Stack";
 import Popover from "react-bootstrap/Popover";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import segment from "./sentencex";
 
+// A modal editor for setting/editing the highlight summary
 
-const DocSentence = ({  key, sentence, judgment, add_passage, del_passage, children  }) => {
+const EditorDialog = ({show, setShow, the_note, the_vital, doSave}) => {
+  const [note, setNote] = useState("");
+  const [vital, setVital] = useState(false);
+
+  useEffect(() => {
+    if (show) {
+      setNote(the_note);
+      setVital(the_vital);
+      console.log('showing editor', the_note, the_vital);
+    }
+  }, [show, the_note, the_vital]);
+
+  return (
+    <Modal show={show} onHide={() => setShow(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>Edit Summary</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Control
+            type="text"
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+          />
+          <Form.Check
+            type="checkbox"
+            label="Vital?"
+            checked={vital}
+            onClick={() => setVital(!vital)}
+          />
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => setShow(false)}>
+          Cancel
+        </Button>
+        <Button
+          variant="primary"
+          onClick={() => {
+            doSave(note, vital);
+            setShow(false);
+          }}
+        >
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+EditorDialog.propTypes = {
+  show: PropTypes.bool,
+  setShow: PropTypes.func,
+  the_note: PropTypes.string, 
+  the_vital: PropTypes.string, 
+  doSave: PropTypes.func,
+}
+
+// An individual sentence in the document.
+// You can click on it to highlight it, and that brings up the summary editor.
+// After it's been clicked, hovering shows a Popover with the current summary
+// The Popover has an edit button that brings up the editor dialog.
+// Sentences, they be busy.
+const DocSentence = ({
+  key,
+  sentence,
+  judgment,
+  add_passage,
+  del_passage,
+  children,
+}) => {
   const [highlight, setHighlight] = useState(false);
   const [showNote, setShowNote] = useState(false);
   const [noteText, setNoteText] = useState(judgment ? judgment.note : "");
   const [isVital, setIsVital] = useState(judgment ? judgment.vital : false);
-  
+  const [showEditor, setShowEditor] = useState(false);
+
   const build_passage = () => {
-    return { sentence: sentence, text: children, vital: isVital, note: noteText};
+    return {
+      sentence: sentence,
+      text: children,
+      vital: isVital,
+      note: noteText,
+    };
   };
 
   const popover = (
-    <Popover 
-      id="note" 
-      onMouseEnter={() => setShowNote(true)} 
-      onMouseLeave={() => setShowNote(false)} 
-      style={{ width: '500px' }}
-      >
-      <Popover.Header as="h3">Enter note</Popover.Header>
+    <Popover
+      id="note"
+      onMouseEnter={() => setShowNote(true)}
+      onMouseLeave={() => setShowNote(false)}
+      style={{ width: "500px" }}
+    >
       <Popover.Body>
-        <Form.Group as={Row} className="align-items-center">
-          <Col>
-         <Form.Control type="text" value={noteText} onChange={(e) => setNoteText(e.target.value)}/>
-         </Col>
-         <Col className="col-auto">
-         <Form.Check label="Vital?" checked={isVital} onClick={() => setIsVital(!isVital)}/>
-         </Col>
-         <Col className="col-auto">
-         <Button onClick={() => { add_passage(build_passage()); setShowNote(false) }}>Done</Button>
-         </Col>
-         </Form.Group>
+        <Stack direction="horizontal" gap={3}>
+          <div className="p-2">
+            {noteText} {isVital ? <strong>(vital)</strong> : ""}
+          </div>
+          <div className="ms-auto">
+            <Button onClick={() => setShowEditor(true)}>Edit</Button>
+          </div>
+        </Stack>
       </Popover.Body>
     </Popover>
   );
@@ -59,26 +133,40 @@ const DocSentence = ({  key, sentence, judgment, add_passage, del_passage, child
       del_passage(build_passage());
     } else {
       setHighlight(true);
-      setShowNote(true);
+      setShowEditor(true);
       add_passage(build_passage());
     }
   };
 
+  const save_update = (note, vital) => {
+    setNoteText(note);
+    setIsVital(vital);
+    add_passage(build_passage);
+  };
+
   return (
-    <OverlayTrigger 
-      trigger={undefined} 
-      show={showNote} 
-      placement="auto"
-      overlay={popover}>
-      <span
-        style={{ backgroundColor: highlight ? "yellow" : "inherit" }}
-        onClick={handleClick}
-        onMouseEnter={() => { if (highlight && !showNote) setShowNote(true) }}
-        onMouseLeave={() => { if (highlight && showNote) setShowNote(false)}}
+    <>
+      <EditorDialog show={showEditor} setShow={setShowEditor} note={noteText} vital={isVital} doSave={save_update}/>
+      <OverlayTrigger
+        trigger={undefined}
+        show={showNote}
+        placement="auto"
+        overlay={popover}
       >
-        {children}
-      </span>
-    </OverlayTrigger>
+        <span
+          style={{ backgroundColor: highlight ? "yellow" : "inherit" }}
+          onClick={handleClick}
+          onMouseEnter={() => {
+            if (highlight && !showNote) setShowNote(true);
+          }}
+          onMouseLeave={() => {
+            if (highlight && showNote) setShowNote(false);
+          }}
+        >
+          {children}
+        </span>
+      </OverlayTrigger>
+    </>
   );
 };
 
@@ -91,14 +179,21 @@ DocSentence.propTypes = {
   children: PropTypes.node,
 };
 
-export default function DocumentView({ document, judgment, add_passage, del_passage }) {
+export default function DocumentView({
+  document,
+  judgment,
+  add_passage,
+  del_passage,
+}) {
   const [sentences, setSentences] = useState([]);
   const [judgeMap, setJudgeMap] = useState(new Map());
-  
+
   useEffect(() => {
     if (judgment) {
       const jmap = new Map();
-      judgment.forEach((j) => { jmap.set(j.sentence, j); });
+      judgment.forEach((j) => {
+        jmap.set(j.sentence, j);
+      });
       // console.log("jmap is", jmap);
       setJudgeMap(jmap);
     }
@@ -112,7 +207,10 @@ export default function DocumentView({ document, judgment, add_passage, del_pass
       rus: "ru",
       zho: "zh",
     };
-    const lang = (document && document["lang"] in lang_map) ? lang_map[document["lang"]] : "en";
+    const lang =
+      document && document["lang"] in lang_map
+        ? lang_map[document["lang"]]
+        : "en";
     let sentences = document ? segment(lang, document.text) : [];
     sentences = sentences.map((s) => `${s} `);
     // console.log("sentences are", sentences);
@@ -133,9 +231,13 @@ export default function DocumentView({ document, judgment, add_passage, del_pass
       </div>
       <div dir={textdir} className={textclass}>
         {sentences.map((sent, index) => (
-          <DocSentence key={index} sentence={index} 
+          <DocSentence
+            key={index}
+            sentence={index}
             judgment={judgeMap.has(index) ? judgeMap.get(index) : null}
-            add_passage={add_passage} del_passage={del_passage}>
+            add_passage={add_passage}
+            del_passage={del_passage}
+          >
             {sent}
           </DocSentence>
         ))}{" "}
@@ -151,4 +253,3 @@ DocumentView.propTypes = {
   add_passage: PropTypes.func,
   del_passage: PropTypes.func,
 };
-  
