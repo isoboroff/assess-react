46a025a0015d7ce7ce3bb1d5d916d93c77cdff22
soboroff <ian.soboroff@nist.gov>
2025-08-19 11:24:00 -0400

    Update description for sub-narratives

--- front-end/src/Description.jsx
+++ front-end/src/Description.jsx
@@ -3,13 +3,17 @@ import PropTypes from "prop-types";
 // Render the task/request description
 function Description(props) {
   if (props.desc) {
+    const subnarratives = props.desc['sub_narratives'].map((n) => <li>{n['text']} ({n['importance']})</li>)
     return (
       <div className="border-bottom">
         <span className="h2 mr-5">Request: {props.desc["id"]}</span>
         <br />
         <p>
-          <b>{props.desc["narrative"]}</b>
+          {props.desc["narrative"]}
         </p>
+        <ul>
+          {subnarratives}
+        </ul>
       </div>
     );
   } else {
